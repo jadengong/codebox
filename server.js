c67be1953352ca00c9c6048263ea5a450c7f5281
Jaden Ong <ong.jadengabriel@gmail.com>
2025-03-29 17:33:00 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+// Load env variables from .env file
+dotenv.config();
+
+const app = express(); // Create Express application
+const port = process.env.PORT || 3000; // Set port from environment or default 
+
+// To allow cross-origin requests
+app.use(cors());
+
+// To parse incoming JSON requests
+app.use(express.json());
+
+// Define router handler for GET requests at root URL "/"
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start server and listen for requests
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
