9d6e3b2606e514843d5b22dbd232661f690c5e10
Jaden Ong <ong.jadengabriel@gmail.com>
2025-09-01 18:57:49 -0700

    fix api 404 error - convert CommonJS and simplify Vercel config

--- api/execute.js
+++ api/execute.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   console.log('[API] Execute function called:', {
     method: req.method,
     url: req.url,
